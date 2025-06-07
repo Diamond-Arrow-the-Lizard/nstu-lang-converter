0d3d668e36bd5f8ddd6a4a259ec9355b1e5b5c05
Diamond_Arrow <diamond1arrow@gmail.com>
2025-06-07 20:26:45 +0700

    Changed: CLI testing program: uses DI, parsing text overhauled

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Runtime.CompilerServices;
+﻿using Microsoft.Extensions.DependencyInjection; 
 using Core.Parser.Handlers.TextToTokenHandlers;
 using Core.Parser.Handlers.TextToTokenHandlers.KeywordTextToTokenHandlers;
 using Core.Parser.Handlers.TextToTokenHandlers.KeywordTextToTokenHandlers.ControlFlowHandlers.IfElseHandlers;
@@ -10,9 +9,14 @@ using Core.Parser.Handlers.TextToTokenHandlers.OperationTextToTokenHandlers;
 using Core.Parser.Handlers.TextToTokenHandlers.VariableTypeTextToTokenHandlers;
 using Core.Parser.Interfaces.Handlers;
 using Core.Parser.Interfaces.Models;
+using Core.Parser.Interfaces.Repositories; 
+using Core.Parser.Interfaces.Services; 
 using Core.Parser.Models;
 using Core.Parser.Repositories;
 using Core.Parser.Services;
+using Core.Parser.AST.ASTParser; 
+using Core.Parser.AST.Nodes;
+using Core.Parser.Interfaces.AST;
 
 namespace CLI;
 
@@ -20,7 +24,9 @@ public static class CLI
 {
     public static void Main()
     {
-        List<ITextToTokenHandler> textToTokenHandlers =
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddSingleton<IEnumerable<ITextToTokenHandler>>(sp =>
         [
             new ProgramBeginTextToTokenHandler(),
             new ProgramEndTextToTokenHandler(),
@@ -52,30 +58,91 @@ public static class CLI
 
             new ReadKeywordTextToTokenHandler(),
             new WriteKeywordTextToTokenHandler(),
+        ]);
 
-        ];
-        TokenService tokenService = new();
-        TokenRepository tokenRepository = new(tokenService);
-        StringParser parser = new(tokenRepository, textToTokenHandlers);
+        serviceCollection.AddSingleton<ITokenService, TokenService>();
+        serviceCollection.AddSingleton<ITokenRepository, TokenRepository>();
+        serviceCollection.AddSingleton<IStringParser, StringParser>();
+        serviceCollection.AddSingleton<IParser, Parser>();
 
-        string textToParse = "начало цел X = 1 + 1 нц 5 раз кц если 1 + 1 == 2 то написать \"Афифметика верная\"; кесли иначе написать \"Hello, World\"; кесли вернуть 0; конец".TrimEnd();
+        // Build the service provider
+        var serviceProvider = serviceCollection.BuildServiceProvider();
 
-        Console.WriteLine("Text to parse:");
-        foreach(var i in textToParse)
+        // 2. Comprehensive Pseudo-Language Code String
+        string textToParse = @"
+начало
+    цел Num1 = 5;
+    плав DecVal = 10.5;
+    строка Message = ""Привет, мир!"";
+
+    написать ""Начальные значения:"";
+    написать Num1;
+    написать DecVal;
+    написать Message;
+
+    Num1 = Num1 + 2 * 3;     
+    DecVal = DecVal / 2.0 - 1.5; 
+    написать ""Новые значения:"";
+    написать Num1;
+    написать DecVal;
+
+    прочитать Message;       
+    написать ""Вы ввели: "";
+    написать Message;
+
+    если Num1 == 11 то      
+        написать ""Num1 стало 11!"";
+    иначе                   
+        написать ""Num1 не 11."";
+    кесли;                  
+
+    нц 2 раз                
+        написать ""Повтор внутри цикла!"";
+        цел Counter = 1;
+        Counter = Counter + 1; 
+    кц;                     
+
+    вернуть 0;              
+конец
+".Trim(); // Trim to remove leading/trailing whitespace from the multiline string
+
+        Console.WriteLine("--- Text to parse ---");
+        Console.WriteLine(textToParse);
+        Console.WriteLine("---------------------\n");
+
+        // 3. Get services and execute parsing
+        var stringParser = serviceProvider.GetRequiredService<IStringParser>();
+        var parser = serviceProvider.GetRequiredService<IParser>();
+        var tokenRepository = serviceProvider.GetRequiredService<ITokenRepository>();
+
+
+        stringParser.SetText(textToParse);
+        stringParser.MakeTokenizedExpression();
+
+        List<IToken> tokens = tokenRepository.GetAllTokens();
+        Console.WriteLine("--- Tokenization Result ---");
+        foreach(var token in tokens)
         {
-            Console.Write($"{i}");
+            Console.WriteLine($"Token type:{token.TokenType}, representation: '{token.Representation}'");
         }
-        Console.WriteLine();
+        Console.WriteLine("---------------------------\n");
 
-        parser.SetText(textToParse);
-        parser.MakeTokenizedExpression();
-
-        List<IToken> result = tokenRepository.GetAllTokens();
-        Console.WriteLine("Parse result:");
-        foreach(var token in result)
+        Console.WriteLine("--- AST Parsing Result ---");
+        try
         {
-            Console.WriteLine($"Token type:{token.TokenType}, string representation: {token.Representation}");
+            ProgramNode ast = parser.Parse();
+            Console.WriteLine("AST built successfully!");
+            // You can print the debug string of the root node to see the AST structure
+            Console.WriteLine(ast.ToDebugString());
         }
-
+        catch (SyntaxException ex)
+        {
+            Console.WriteLine($"Syntax Error: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+        }
+        Console.WriteLine("--------------------------\n");
     }
 }
